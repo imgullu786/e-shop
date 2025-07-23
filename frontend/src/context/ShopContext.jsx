05a02cea42bb588200edc86d58722ab290c653a4
Md Gulam Gaush <gulamgaush.dev@gmail.com>
2025-07-23 12:18:45 +0530

    Update backend url

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -9,7 +9,7 @@ const ShopContextProvider = (props) => {
 
     const currency = '$';
     const delivery_fee = 10;
-    const backendUrl = "https://eshop-backend-dtly.onrender.com/"
+    const backendUrl = "https://eshop-backend-dtly.onrender.com"
     const [search, setSearch] = useState('');
     const [showSearch, setShowSearch] = useState(false);
     const [cartItems, setCartItems] = useState({});
