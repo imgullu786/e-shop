0819c8df91de44ed5ae41705d2d6b00d3f72adc6
Md Gulam Gaush <123100963+imgullu786@users.noreply.github.com>
2025-07-23 12:01:19 +0530

    cron added

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import job from './cron/cron.js'
 
 import userRouter from './routes/userRouter.js'
 import productRouter from './routes/productRouter.js'
 import cartRouter from './routes/cartRouter.js'
 // import orderRouter from './routes/orderRouter.js'
 
-
+job.start()
 const app = express()
 const PORT = process.env.PORT || 4000
 connectCloudinary()
